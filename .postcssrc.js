7247170556560a61f7a4cdbd2d66cd228a2fcb27
jianfengtheboy <jianfengtheboy@163.com>
2018-10-29 18:42:51 +0800

    renew in 10.29 18:42pm

--- .postcssrc.js
+++ .postcssrc.js
@@ -5,6 +5,20 @@ module.exports = {
     "postcss-import": {},
     "postcss-url": {},
     // to edit target browsers: use "browserslist" field in package.json
-    "autoprefixer": {}
+    "autoprefixer": {},
+    "postcss-aspect-ratio-mini" : {},
+    "postcss-write-svg" : {
+        utf8 : false
+    },
+    "postcss-cssnext" : {},
+    "postcss-px-to-viewport" : {
+        viewportWidth: 750,
+        unitPrecision: 5,
+        viewportUnit: 'vw',
+        selectorBlackList: [],
+        minPixelValue: 1,
+        mediaQuery: false
+    },
+    "postcss-viewport-units" : {}
   }
 }
--- src/App.vue
+++ src/App.vue
@@ -10,5 +10,4 @@ export default {
 }
 </script>
 
-<style lang="scss" type="text/css">
-</style>
+<style lang="scss" type="text/css"></style>
