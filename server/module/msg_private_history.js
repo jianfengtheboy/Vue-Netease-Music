c7a4ac3031b3d932cf96182630fdfd3b7c888f7f
jianfengtheboy <jianfengtheboy@163.com>
2019-05-13 23:37:16 +0800

    更新接口

--- server/module/msg_private_history.js
+++ server/module/msg_private_history.js
@@ -1,16 +1,15 @@
-// 评论
+// 私信内容
 
 module.exports = (query, request) => {
   const data = {
-    beforeTime: query.beforeTime || "-1",
+    userId: query.uid,
+    offset: query.offset || 0,
     limit: query.limit || 30,
-    total: "true",
-    uid: query.uid
+    total: "true"
   };
-  // 448109360
   return request(
     "POST",
-    `https://music.163.com/api/v1/user/comments/${query.uid}`,
+    `https://music.163.com/api/msg/private/history`,
     data,
     {
       crypto: "weapi",
